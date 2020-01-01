3eba89b819cc6593e381dab8dd0406e150f6a7de
jionie <zcao76@gatech.edu>
2020-01-02 01:09:13 +0900

    update hyperparameters

--- bert-uncased-k-fold.sh
+++ bert-uncased-k-fold.sh
@@ -1,7 +1,10 @@
-python training-k-fold.py --model_name "bert-base-uncased" --fold 0 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 1 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 2 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 3 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 4 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 5 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
-python training-k-fold.py --model_name "bert-base-uncased" --fold 6 --seed 42 --n_splits 7 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-3 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 0 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 1 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 2 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 3 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 4 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 5 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 6 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 7 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 8 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
+python training-k-fold.py --model_name "bert-base-uncased" --fold 9 --seed 42 --n_splits 10 --batch_size 16 --valid_batch_size 32 --accumulation_steps 2 --lr 5e-5 --loss "bce" --augment --num_epoch 20
