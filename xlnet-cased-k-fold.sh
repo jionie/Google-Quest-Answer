d097deb7e3c96ce0561e0f66a032ee0e4a42c3b2
jionie <jionie@sjtu.edu.cn>
2020-01-30 21:59:53 -0500

    update .sh files

--- xlnet-cased-k-fold.sh
+++ xlnet-cased-k-fold.sh
@@ -1,14 +1,7 @@
-python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 0 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 1 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 2 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 3 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 4 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-# python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 5 --seed 1997 --n_splits 10 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-4 --loss "bce" --augment --num_epoch 13 --num_workers 4
-# python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 6 --seed 1997 --n_splits 10 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-4 --loss "bce" --augment --num_epoch 13 --num_workers 4
-# python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 7 --seed 1997 --n_splits 10 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-4 --loss "bce" --augment --num_epoch 13 --num_workers 4
-# python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 8 --seed 1997 --n_splits 10 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-4 --loss "bce" --augment --num_epoch 13 --num_workers 4
-# python training-k-fold.py --model_type "xlnet" --model_name "xlnet-base-cased" --fold 9 --seed 1997 --n_splits 10 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 2e-4 --loss "bce" --augment --num_epoch 13 --num_workers 4
-
-
+python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-base-cased" --fold 0 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
+python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-base-cased" --fold 1 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
+python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-base-cased" --fold 2 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
+python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-base-cased" --fold 3 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
+python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-base-cased" --fold 4 --seed 1997 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
 
 
