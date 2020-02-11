ec865c6283a594f78d31ad9f04d93b95060ebefd
jionie <jionie@sjtu.edu.cn>
2020-02-10 21:16:08 -0500

    clean useless codes

--- flaubert-cased-k-fold.sh
+++
@@ -1,6 +0,0 @@
-python training-k-fold.py --model_name "flaubert-base-cased" --content "Question_Answer" --fold 0 --seed 666 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_name "flaubert-base-cased" --content "Question_Answer" --fold 1 --seed 666 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_name "flaubert-base-cased" --content "Question_Answer" --fold 2 --seed 666 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_name "flaubert-base-cased" --content "Question_Answer" --fold 3 --seed 666 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_name "flaubert-base-cased" --content "Question_Answer" --fold 4 --seed 666 --n_splits 5 --batch_size 4 --valid_batch_size 32 --accumulation_steps 2 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-
--- xlnet-large-cased-k-fold.sh
+++
@@ -1,8 +0,0 @@
-python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-large-cased" --fold 0 --seed 666 --n_splits 5 --batch_size 2 --valid_batch_size 16 --accumulation_steps 4 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-large-cased" --fold 1 --seed 666 --n_splits 5 --batch_size 2 --valid_batch_size 16 --accumulation_steps 4 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-large-cased" --fold 2 --seed 666 --n_splits 5 --batch_size 2 --valid_batch_size 16 --accumulation_steps 4 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-large-cased" --fold 3 --seed 666 --n_splits 5 --batch_size 2 --valid_batch_size 16 --accumulation_steps 4 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-python training-k-fold.py --model_type "xlnet" --content "Question_Answer" --model_name "xlnet-large-cased" --fold 4 --seed 666 --n_splits 5 --batch_size 2 --valid_batch_size 16 --accumulation_steps 4 --lr 1e-4 --loss "bce" --augment --num_epoch 8 --num_workers 4
-
-
-
